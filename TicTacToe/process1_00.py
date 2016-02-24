87a4901253841ce4d7365c0829beb76353a5a7f4
harshnarang8 <harshnarang8@gmail.com>
2016-02-24 12:25:57 +0530

    basic minimax added
    minimax addition taken place but the tree implementation for nodes is
    left
    heuristic value calculation is also left

--- TicTacToe/process1_00.py
+++ TicTacToe/process1_00.py
@@ -4,12 +4,34 @@
 #
 #################
 
+import Decimal
+
+PosInf = Decimal('Infinity')
+NegInf = Decimal('-Infinity')
+
 class AI(object) :
 
     def __init__(self) :
         self.board = [[0 for i in range(3)] for j in range(3)]
         pass
 
+    def minimax(self, node, depth, maxPlayer) :
+        if depth == 0 or term(node) :
+            return heuristic(node)
+
+        if maxPlayer :
+            bestValue = NegInf
+            for each child of node #to be corrected
+                v = self.minimax(self,child, depth - 1, False)
+                bestValue = v if v > bestValue else bestValue
+            return bestValue
+
+        else :
+            bestValue = PosInf
+            for each cild of node #to be corrected
+                v = self.minimax(self,child, depth - 1, True)
+                bestValue = v if v < bestValue else bestValue
+            return bestValue
     def NextMove(self) :
         # Use MiniMax Algorithm for calculating next move
         # return best move
